20dca9d4e827b3a4b371f504053389c56db2ae2b
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2024-12-18 21:05:24 +0100

    Update mlxwin_init.c

--- src/ray_tracing/mlxwin_init.c
+++ src/ray_tracing/mlxwin_init.c
@@ -68,3 +68,58 @@ void	win_handle(t_rt *rt, char *path)
 	check_mlx_call(rt->img.addr, rt);
 	rt->is_processing = FALSE;
 }
+
+/*
+static void	ptr_null(void **pt)
+{
+	if (*pt)
+		free(*pt);
+	*pt = NULL;
+}
+
+void	rt_clear(t_rt *rt)
+{
+	// if (rt->objs->texture.img)
+	// {
+	// 	rt->objs->texture.img = NULL;
+	// 	rt->objs->has_texture = FALSE;
+	// 	free(rt->objs->texture.path);
+	// }
+	if (rt->main_win)
+		mlx_destroy_window(rt->mlx, rt->main_win);
+	if (rt->img.img)
+		mlx_destroy_image(rt->mlx, rt->img.img);
+	if (rt->mlx)
+	{
+		mlx_destroy_display(rt->mlx);
+		ptr_null(&rt->mlx);
+	}
+	rt_free(rt);
+	exit(0);
+}
+
+void	win_handle(t_rt *rt, char *path)
+{
+	rt->mlx = mlx_init();
+	if (rt->width <= 0 || rt->height <= 0)
+	{
+		rt->width = (float) WIN_WIDTH;
+		rt->height = (float) WIN_HEIGHT;
+	}
+	if (rt->height < rt->width)
+		rt->aspectratio = rt->width / rt->height;
+	else
+		rt->aspectratio = rt->height / rt->width;
+	rt->main_win = mlx_new_window(rt->mlx, rt->width, \
+		rt->height, "miniRT our awesome joint project");
+	rt->path = path;
+	rt->img.img = mlx_new_image(rt->mlx, rt->width, rt->height);
+	rt->img.addr = mlx_get_data_addr(rt->img.img, &rt->img.bits_per_pixel,
+			&rt->img.line_length, &rt->img.endian);
+	rt->img.antialiasing_on = ANTIALIAS_ON;
+	if (!rt->img.img)
+		rt_clear(rt);
+	rt->img.addr_incr = rt->img.bits_per_pixel / 8;
+	rt->is_processing = FALSE;
+}
+*/
